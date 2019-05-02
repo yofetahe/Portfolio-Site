18f146b9188270ecf33a555e44d5076f565cece4
Yofatehe <42419407+yofetahe@users.noreply.github.com>
2019-05-01 21:15:45 -0700

    model migrations

--- apps/myprofile/models.py
+++ apps/myprofile/models.py
@@ -1,11 +1,10 @@
 from django.db import models
 
 # Create your models here.
-class MyNote(models.Manager):
+class MyNote(models.Model):
     title = models.CharField(max_length=225)
     intro_info = models.TextField()
     categoty = models.CharField(max_length=225)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     udpated_at = models.DateTimeField(auto_now=True)
-    
